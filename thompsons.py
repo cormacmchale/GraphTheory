fd805cc75f0f6c647067bf8712cf67e26ed45d2f
unknown <cormacmchale1@gmail.com>
2019-03-19 20:48:58 +0000

    thompsons explaination added

--- thompsons.py
+++ thompsons.py
@@ -21,11 +21,15 @@ def compile(postfix):
 
 
     for c in postfix:
+        #when you encounter the . operator, pop the two smaller NFA's of the statck and concatenates them into one
+        #by making the second nfa point towards the initial state of the first nfa that was popped off the stack
         if c=='.':
             nfa2 = nfastack.pop()
             nfa1 = nfastack.pop()
             nfa1.accept.edge1 = nfa2.initial
             nfastack.append(nfa(nfa1.initial, nfa2.accept))
+        #when you encounter the | operator again pop the two nfa's that are on the stack off only this time you
+        #initilise an initial state that points towards both NFA's and have both NFA's point towards an accept state
         elif c =='|':
             nfa2 = nfastack.pop()
             nfa1 = nfastack.pop()
@@ -36,6 +40,9 @@ def compile(postfix):
             nfa1.accept.edge1 = accept
             nfa2.accept.edge2 = accept
             nfastack.append(nfa(initial,accept))
+        #when you encounter the * operator you initilise an initial state and have it point towards the accept state 
+        #and the NFA, you must also make the NFA point towards the accept state and also loop back towrads itselt to represent 
+        #the empty string and the repeating of characters
         elif c =='*':
             nfa1 = nfastack.pop()
             initial = state()
@@ -44,14 +51,16 @@ def compile(postfix):
             initial.edge2 = accept
             nfa1.accept.edge1 = nfa1.initial
             nfa1.accept.edge2 = accept
-            nfastack.append(nfa(initial, accept))   
+            nfastack.append(nfa(initial, accept))  
+        #every time you read a regular character on the charcter array postfix stack then add in the accept state for 
+        #reading that character in an NFA 
         else:
             accept = state()
             initial = state()
             initial.label = c
             initial.edge1 = accept
             nfastack.append(nfa(initial,accept))
-     
+    #return the final NFA which is the only thing in the array
     return nfastack.pop()
 
 print(compile(shunt("(a.b)|(c*.d)")))  
